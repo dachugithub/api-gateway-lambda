ee192bcd7906a13488381dd7f12ceafdc966451b
overlord <overlord@ubuntu-dev.myguest.virtualbox.org>
2023-01-01 19:47:41 +0000

    initial commit

---
+++ hello-world/hello.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+def lambda_handler(event, context):
+    message = 'Hello world'  
+    json_region = os.environ['AWS_REGION']
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "Region ": json_region,
+            "Message": message
+        })
+    }
